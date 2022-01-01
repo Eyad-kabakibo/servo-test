26c242ad3c74ddb05c7c03c46ce1949153d9ec86
Eyad-kabakibo <76591859+Eyad-kabakibo@users.noreply.github.com>
2022-01-01 03:01:51 +0200

    Create Line-follower-robot.ino

---
+++ Line-follower-robot.ino
@@ -0,0 +1,91 @@
+int M1_Speed = 200; 
+int M2_Speed = 200;
+int LeftRotationSpeed = 150;
+int RightRotationSpeed = 150;
+
+ 
+ void setup() {
+  
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  
+  pinMode(enA,OUTPUT);
+  pinMode(enB,OUTPUT);
+  
+  pinMode(A0, INPUT); 
+  pinMode(A1, INPUT); 
+
+}
+ 
+void loop() {
+ 
+ int LEFT_SENSOR = digitalRead(A0);
+ int RIGHT_SENSOR = digitalRead(A1);
+  
+if(RIGHT_SENSOR==0 && LEFT_SENSOR==0) {
+  forward();
+}
+ 
+ else if(RIGHT_SENSOR==0 && LEFT_SENSOR==1) {
+  right(); 
+ }
+ 
+ else if(RIGHT_SENSOR==1 && LEFT_SENSOR==0) {
+  left(); 
+}
+ 
+ else if(RIGHT_SENSOR==1 && LEFT_SENSOR==1) {
+  Stop(); 
+ }
+}
+ 
+ 
+ 
+void forward()
+{
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+     
+      analogWrite(enA, M1_Speed);
+      analogWrite(enB, M2_Speed);
+}
+  
+void backward()
+{
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);       
+      analogWrite(enA, M1_Speed);
+      analogWrite(enB, M2_Speed);
+}
+  
+void right()
+{
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      analogWrite(enB, RightRotationSpeed);
+}
+  
+void left()
+{
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      analogWrite(enA, LeftRotationSpeed);
+}
+  
+void Stop()
+{
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+}
